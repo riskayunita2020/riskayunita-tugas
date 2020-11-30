f6cff6aa18587f4f63c36bfa3ee45bfee87073c0
DonCorleone17 <mukebatu@gmail.com>
2020-11-30 12:50:20 +0700

    Update blog.blade.php

--- system/resources/views/customer/blog.blade.php
+++ system/resources/views/customer/blog.blade.php
@@ -8,7 +8,7 @@
             <div class="row">
               <div class="col-md-12 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch d-md-flex">
-                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_1.jpg');">
+                  <a href="blog-single.html" class="block-20" style="background-image: url({{asset('public/images/image_1.jpg')}});">
                   </a>
                   <div class="text d-block pl-md-4">
                     <div class="meta mb-3">
@@ -24,7 +24,7 @@
               </div>
               <div class="col-md-12 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch d-md-flex">
-                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_2.jpg');">
+                  <a href="blog-single.html" class="block-20" style="background-image: url({{asset('public/images/image_2.jpg')}});">
                   </a>
                   <div class="text d-block pl-md-4">
                     <div class="meta mb-3">
@@ -40,7 +40,7 @@
               </div>
               <div class="col-md-12 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch d-md-flex">
-                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_3.jpg');">
+                  <a href="blog-single.html" class="block-20" style="background-image: url({{asset('public/images/image_3.jpg')}});">
                   </a>
                   <div class="text d-block pl-md-4">
                     <div class="meta mb-3">
@@ -56,24 +56,7 @@
               </div>
               <div class="col-md-12 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch d-md-flex">
-                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_4.jpg');">
-                  </a>
-                  <div class="text d-block pl-md-4">
-                    <div class="meta mb-3">
-                      <div><a href="#">July 20, 2019</a></div>
-                      <div><a href="#">Admin</a></div>
-                      <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-                    </div>
-                    <h3 class="heading"><a href="#">Inim bah ye contohnyen</a></h3>
-                    <h3 class="heading"><a href="#">For the Examples</a></h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Baca selanjutnya</a></p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-12 d-flex ftco-animate">
-                <div class="blog-entry align-self-stretch d-md-flex">
-                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_5.jpg');">
+                  <a href="blog-single.html" class="block-20" style="background-image: url({{asset('public/images/image_4.jpg')}});">
                   </a>
                   <div class="text d-block pl-md-4">
                     <div class="meta mb-3">
@@ -90,7 +73,24 @@
               </div>
               <div class="col-md-12 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch d-md-flex">
-                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_6.jpg');">
+                  <a href="blog-single.html" class="block-20" style="background-image: url({{asset('public/images/image_5.jpg')}});">
+                  </a>
+                  <div class="text d-block pl-md-4">
+                    <div class="meta mb-3">
+                      <div><a href="#">July 20, 2019</a></div>
+                      <div><a href="#">Admin</a></div>
+                      <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
+                    </div>
+                    <h3 class="heading"><a href="#">Inim bah ye contohnyen</a></h3>
+                    <h3 class="heading"><a href="#">For the Examples</a></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Baca selanjutnya</a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-12 d-flex ftco-animate">
+                <div class="blog-entry align-self-stretch d-md-flex">
+                  <a href="blog-single.html" class="block-20" style="background-image: url({{asset('public/images/image_6.jpg')}});">
                   </a>
                   <div class="text d-block pl-md-4">
                     <div class="meta mb-3">
@@ -121,14 +121,14 @@
                 <li><a href="#">Barang<span>(12)</span></a></li>
                 <li><a href="#">Jasa<span>(22)</span></a></li>
                 <li><a href="#">Toko<span>(37)</span></a></li>
-                <li><a href="#">Pemilu Bupati 2020<span>(42)</span></a></li>
+                <li><a href="#">Pengiriman<span>(42)</span></a></li>
               </ul>
             </div>
 
             <div class="sidebar-box ftco-animate">
               <h3 class="heading">Baru-baru ini</h3>
               <div class="block-21 mb-4 d-flex">
-                <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
+                <a class="blog-img mr-4" style="background-image: url({{asset('public/images/image_1.jpg')}});"></a>
                 <div class="text">
                   <h3 class="heading-1"><a href="#">The standard Lorem Ipsum passage, used since the 1500s</a></h3>
                   <div class="meta">
@@ -139,7 +139,7 @@
                 </div>
               </div>
               <div class="block-21 mb-4 d-flex">
-                <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
+                <a class="blog-img mr-4" style="background-image: url({{asset('public/images/image_2.jpg')}});"></a>
                 <div class="text">
                   <h3 class="heading-1"><a href="#">The standard Lorem Ipsum passage, used since the 1500s</a></h3>
                   <div class="meta">
@@ -150,7 +150,7 @@
                 </div>
               </div>
               <div class="block-21 mb-4 d-flex">
-                <a class="blog-img mr-4" style="background-image: url(images/image_3.jpg);"></a>
+                <a class="blog-img mr-4" style="background-image: url({{asset('public/images/image_3.jpg')}});"></a>
                 <div class="text">
                   <h3 class="heading-1"><a href="#">The standard Lorem Ipsum passage, used since the 1500s</a></h3>
                   <div class="meta">
